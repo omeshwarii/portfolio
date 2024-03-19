ad0b482b71eccd8ab03b96d658f67b546a15c36a
Omeshwari Jakkula <jakko01@pfw.edu>
2024-03-19 10:43:38 -0400

    recent changes

--- portfolio_frontend/src/components/Intro/intro.js
+++ portfolio_frontend/src/components/Intro/intro.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import './intro.css'
 import linkedinImg from '../../assets/linkedin-icon.png'
-import gitImg from '../../assets/git-icon.png'
+import gitImg from '../../assets/git-icon2.png'
 
 const Intro = () => {
   return (
     <section id="intro">
       <div className="introContent">
         <span className="hello">Hello</span>
-        <span className="introText">I'm <span className="introName">Omeshwari Jakkula</span></span>
-        <p className="introPara">I'm a recent graduate with a masters degree in Computer Science, actively looking for full time opportunites</p>
-      </div>
+        <span className="introText">I'm < span className="introName">Omeshwari Jakkula</span></span>
+        <p className="introPara">I'm a recent graduate with a masters degree in Computer Science, <br/>actively looking for full time opportunites</p>
       
-      <a href="https://github.com/omeshwarii">
+      <div className="links">
+      <a href="https://github.com/omeshwarii" className="linkItem">
       <img src={gitImg} alt="GitHub" className="gitImg" />
       </a>
-      <a href="https://www.linkedin.com/in/omeshwarijakkula/">
+      <a href="https://www.linkedin.com/in/omeshwarijakkula/" className="linkItem">
       <img src={linkedinImg} alt="LinkedIn" className="linkedinImg" />
       </a>
+      </div>
+      </div>
     </section>
   )
 }
--- portfolio_frontend/src/components/Navbar/navbar.js
+++ portfolio_frontend/src/components/Navbar/navbar.js
@@ -3,10 +3,16 @@ import './navbar.css'
 import {Link} from 'react-scroll'
 
 const Navbar = () => {
+  const scrollToLine = () => {
+    const lineElement = document.getElementById('line');
+    if (lineElement) {
+      lineElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
           <nav className="navbar">  
             <div className="desktopMenu">
-                <Link to="projects" className="desktopMenuListItem">Projects</Link>
+                <Link to="projects" onClick={scrollToLine} className="desktopMenuListItem">Projects</Link>
                 <Link to="skills" className="desktopMenuListItem">Skills</Link>
                 <Link to="about" className="desktopMenuListItem">About</Link>
                 <Link to="contact" className="desktopMenuListItem">Contact</Link>
